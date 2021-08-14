3dbd2be1de5d3072f1230d555e06863d3f77c041
Paweł Zaremba <zarembapawel95@gmail.com>
2021-08-14 14:22:40 +0200

    initial commit

---
+++ src/main/java/com/zarembapawel/FuelCosts/FuelCostsApplication.java
@@ -0,0 +1,13 @@
+package com.zarembapawel.FuelCosts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelCostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelCostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zarembapawel/FuelCosts/FuelCostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zarembapawel.FuelCosts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelCostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
